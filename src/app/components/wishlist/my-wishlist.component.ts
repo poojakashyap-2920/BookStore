96de73f33ab795fc2d1a8b443b2827c970868ad0
poojakashyap-2920 <pooja337026@gmail.com>
2024-05-22 21:44:43 +0530

    getallwishlist and removewishlish api call is done

--- src/app/components/wishlist/my-wishlist.component.ts
+++ src/app/components/wishlist/my-wishlist.component.ts
@@ -11,13 +11,26 @@ import { CartserviceService } from '../service/cartService/cartservice.service';
 export class MyWishlistComponent implements OnInit, OnDestroy {
   sendMyWishlistDetail: BookObj[] = [];
   wishlistSubscription!: Subscription;
+  isLoggedIn: boolean = false;
 
   constructor(private cartService: CartserviceService) {}
 
   ngOnInit(): void {
-    this.wishlistSubscription = this.cartService.wishlistItems$.subscribe(wishlistItems => {
-      this.sendMyWishlistDetail = wishlistItems;
-    });
+    this.isLoggedIn = localStorage.getItem('AuthToken') !== null;
+
+    if (this.isLoggedIn) {
+      this.cartService.getAllWishListApi().subscribe(
+        res => {
+          this.sendMyWishlistDetail = res.data; // Update this line to access the `data` field
+          this.cartService.saveWishlistItemsToStorage(this.sendMyWishlistDetail);
+        },
+        err => console.log(err)
+      );
+    } else {
+      this.wishlistSubscription = this.cartService.wishlistItems$.subscribe(wishlistItems => {
+        this.sendMyWishlistDetail = wishlistItems;
+      });
+    }
   }
 
   ngOnDestroy(): void {
@@ -26,7 +39,31 @@ export class MyWishlistComponent implements OnInit, OnDestroy {
     }
   }
 
-  removeFromCart(book: BookObj) {
-    this.cartService.removeFromWishlist(book);
+  removeFromWishlist(book: BookObj) {
+    if (this.isLoggedIn) {
+      this.cartService.removeWishListApiCall(book.BookId!).subscribe(
+        () => {
+          this.cartService.removeFromWishlist(book);
+          this.updateWishlistDetail();
+          console.log('Item removed from backend and local wishlist:', book);
+        },
+        err => {
+          console.error('Error removing item from wishlist:', err);
+          if (err.status === 401) {
+          } else if (err.status === 404) {
+            console.error('Wishlist item not found in backend');
+          }
+        }
+      );
+    } else {
+      this.cartService.removeFromWishlist(book);
+      this.updateWishlistDetail();
+      console.log('Item removed from local wishlist:', book);
+    }
   }
+
+  updateWishlistDetail() {
+    this.sendMyWishlistDetail = this.cartService.getWishlistItemsFromStorage();
+  }
+  
 }
